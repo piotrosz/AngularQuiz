af285b9bab80021f1a72ce7bfd846e10149dcb8e
Piotr Ludwiczuk <piotrosz@gmail.com>
2014-02-24 16:18:57 +0100

    Added Ninject

---
+++ SimpleQuiz.Core/Ninject/CoreModule.cs
@@ -0,0 +1,21 @@
+﻿using Ninject.Modules;
+using SimpleQuiz.CheckAnswerStrategy;
+using SimpleQuiz.Core.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuiz.Core.Ninject
+{
+    public class CoreModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+            Bind<IQuizAnswerChecker>().To<QuizAnswersChecker>();
+            Bind<ICheckAnswerStrategy>().To<AnswerExactStrategy>();
+        }
+    }
+}
