05cf89f4f39cc30a0809924489be91eb5dd1ac59
Piotr Ludwiczuk <piotrosz@gmail.com>
2014-03-26 15:05:20 +0100

    Fixed per controller json formatter bug

--- AngularQuiz.Web/Infrastructure/UserDataOnlyAttribute.cs
+++ AngularQuiz.Web/Infrastructure/UserDataOnlyAttribute.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http.Controllers;
+using System.Net.Http.Formatting;
 
 namespace AngularQuiz.Web.Infrastructure
 {
-    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class UserDataOnlyAttribute : Attribute, IControllerConfiguration
     {
         public void Initialize(HttpControllerSettings controllerSettings, HttpControllerDescriptor controllerDescriptor)
         {
-            var jsonFormatter = controllerSettings.Formatters.JsonFormatter;
+            if (controllerSettings == null)
+            {
+                throw new ArgumentNullException("controllerSettings");
+            }
+
+            if (controllerDescriptor == null)
+            {
+                throw new ArgumentNullException("controllerDescriptor");
+            }
+ 
+            controllerSettings.Formatters.Clear();
+            var jsonFormatter = new JsonMediaTypeFormatter();
             jsonFormatter.SerializerSettings.ContractResolver = new UserDataOnlyContractResolver();
+            jsonFormatter.Indent = true;
+            controllerSettings.Formatters.Add(jsonFormatter);
         }
     }
 }
